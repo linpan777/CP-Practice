920b34ac93f9fa087695ff7eaf5dba970db504e3
Lin Pan <linpan115300@gmail.com>
2026-02-25 20:50:56 +0800

    Update ChristmasTree_v1_primitive.cpp

--- NTOJ/Loop/ChristmasTree/ChristmasTree_v1_primitive.cpp
+++ NTOJ/Loop/ChristmasTree/ChristmasTree_v1_primitive.cpp
@@ -4,17 +4,15 @@ using namespace std;
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0);
-	int input;
+	int input;//列數input
 	cin >> input;
 
 	for (int i{ 1 }; i <= input; i++)
 	{
-		for (int j{ 1 }; j <= input - i; j++)
+		for (int j{ 1 }; j <= input - i; j++)//空格
 			cout << " ";
-		for (int k{ 1 }; k <= 2*i - 1; k++)
+		for (int k{ 1 }; k <= 2*i - 1; k++)//星星
 			cout << '*';
-		for (int l{ 1 }; l <= input - i; l++)
-			cout << " ";
-		cout << '\n';
+		cout << '\n';//每一次都要換行
 	}
 }
