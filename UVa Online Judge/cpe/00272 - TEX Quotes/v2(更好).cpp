a774a0d8e765ee0a5b76a705b59f84a86184ab73
Lin Pan <linpan115300@gmail.com>
2026-02-26 23:39:15 +0800

    Create v2(更好).cpp

---
+++ UVa Online Judge/cpe/00272 - TEX Quotes/v2(更好).cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+	string sentence;
+	bool check = 0;
+
+	while (getline(cin, sentence))
+	{
+		for (int i{ 0 }; i < sentence.size(); i++)
+		{
+			if (sentence[i] == '"')
+				if (check == 0)
+				{
+					cout << "``";
+					check = 1;
+					continue;
+				}
+				else
+				{
+					cout << "''";
+					check = 0;
+					continue;
+				}
+			cout << sentence[i];
+		}
+		cout << '\n';
+	}
+}
