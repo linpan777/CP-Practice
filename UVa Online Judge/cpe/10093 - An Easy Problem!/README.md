ad8298fbb83462ab4588429bf345c9957f7797f0
linpan777 <linpan115300@gmail.com>
2026-03-09 16:52:47 +0800

    update

---
+++ UVa Online Judge/cpe/10093 - An Easy Problem!/README.md
@@ -0,0 +1,18 @@
+# 10093 - An Easy Problem!
+
+## 題目URL -> [here](https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=0&problem=1034&mosmsg=Submission+received+with+ID+31009847)
+
+## Thinking
+
+### variable reference:
+| variable | meaning |
+| :--- | :--- |
+| `number_sy` | Store input string |
+| `temp` | The converted integer value of the current character |
+| `max` | The maximum character value found in the string <sub>determines the starting base</sub> |
+| `total` | The sum of all character values in the string |
+| `value` | A boolean flag to check if a valid base was successfully found |
+
+> converted = 已轉換的
+
+## 邏輯:
---
+++ UVa Online Judge/cpe/10093 - An Easy Problem!/v1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+	string number_sy;
+	
+	while(cin >> number_sy)
+	{	
+		bool value = false;
+		int max{0};
+		int total{0};
+		for(int i{0}; i < number_sy.size(); i++)
+		{
+			int temp{0};
+			
+            //數字換算
+			if(number_sy[i] >= 'A' && number_sy[i] <= 'Z')
+				temp = number_sy[i] - 'A' + 10;
+			else if(number_sy[i] >= 'a' && number_sy[i] <= 'z')
+				temp = number_sy[i] - 'a' + 36;
+			else if(number_sy[i] >= '0' && number_sy[i] <= '9')
+				temp = number_sy[i] - '0';
+				
+			if(max < temp)//決定最少從哪個數字開始猜
+				max = temp;
+				
+			total += temp;//全部相加之後用來判斷
+		}
+		
+		if(max == 0)//預先排除0
+		{
+			cout << 2 << '\n';
+			continue;
+		}
+			
+		for(int n{max + 1}; n <= 62; n++)//core algorithm
+			if(total % (n - 1) == 0)
+			{
+				cout << n << '\n';
+				value = true;
+				break;
+			}
+		
+		if(!value)
+			cout << "such number is impossible!\n";
+		
+	}
+}
